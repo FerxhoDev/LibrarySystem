5dae31ac9c9bbb69bd0e3f973247f8c34b6b6acd
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-03-22 06:16:48 -0600

    Designed finish

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -30,27 +30,204 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.gunaElipsePanel1 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ToggleSwitch1 = new Guna.UI2.WinForms.Guna2ToggleSwitch();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gTxtPass = new Guna.UI2.WinForms.Guna2TextBox();
+            this.gTxtUser = new Guna.UI2.WinForms.Guna2TextBox();
+            this.gunaPictureBox2 = new Guna.UI.WinForms.GunaPictureBox();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gunaElipsePanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // gunaElipse1
             // 
+            this.gunaElipse1.Radius = 8;
             this.gunaElipse1.TargetControl = this;
+            // 
+            // gunaElipsePanel1
+            // 
+            this.gunaElipsePanel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(95)))), ((int)(((byte)(31)))));
+            this.gunaElipsePanel1.Controls.Add(this.gunaPictureBox2);
+            this.gunaElipsePanel1.Controls.Add(this.gunaPictureBox1);
+            this.gunaElipsePanel1.Location = new System.Drawing.Point(474, -2);
+            this.gunaElipsePanel1.Name = "gunaElipsePanel1";
+            this.gunaElipsePanel1.Radius = 8;
+            this.gunaElipsePanel1.Size = new System.Drawing.Size(363, 469);
+            this.gunaElipsePanel1.TabIndex = 0;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 21;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(200)))), ((int)(((byte)(130)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(130, 355);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "Iniciar Sesión";
+            // 
+            // guna2ToggleSwitch1
+            // 
+            this.guna2ToggleSwitch1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ToggleSwitch1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ToggleSwitch1.CheckedState.InnerBorderColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.CheckedState.Parent = this.guna2ToggleSwitch1;
+            this.guna2ToggleSwitch1.Location = new System.Drawing.Point(75, 301);
+            this.guna2ToggleSwitch1.Name = "guna2ToggleSwitch1";
+            this.guna2ToggleSwitch1.ShadowDecoration.Parent = this.guna2ToggleSwitch1;
+            this.guna2ToggleSwitch1.Size = new System.Drawing.Size(35, 20);
+            this.guna2ToggleSwitch1.TabIndex = 2;
+            this.guna2ToggleSwitch1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2ToggleSwitch1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2ToggleSwitch1.UncheckedState.InnerBorderColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.UncheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2ToggleSwitch1.UncheckedState.Parent = this.guna2ToggleSwitch1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(159, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 22);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Mostrar Contraseña";
+            // 
+            // gTxtPass
+            // 
+            this.gTxtPass.BorderRadius = 8;
+            this.gTxtPass.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gTxtPass.DefaultText = "";
+            this.gTxtPass.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.gTxtPass.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.gTxtPass.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gTxtPass.DisabledState.Parent = this.gTxtPass;
+            this.gTxtPass.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gTxtPass.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gTxtPass.FocusedState.Parent = this.gTxtPass;
+            this.gTxtPass.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gTxtPass.HoverState.Parent = this.gTxtPass;
+            this.gTxtPass.IconLeft = global::CapaPresentacion.Properties.Resources.Lock;
+            this.gTxtPass.Location = new System.Drawing.Point(75, 229);
+            this.gTxtPass.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gTxtPass.Name = "gTxtPass";
+            this.gTxtPass.PasswordChar = '\0';
+            this.gTxtPass.PlaceholderForeColor = System.Drawing.Color.White;
+            this.gTxtPass.PlaceholderText = "Contraseña";
+            this.gTxtPass.SelectedText = "";
+            this.gTxtPass.ShadowDecoration.Parent = this.gTxtPass;
+            this.gTxtPass.Size = new System.Drawing.Size(300, 47);
+            this.gTxtPass.TabIndex = 17;
+            this.gTxtPass.UseSystemPasswordChar = true;
+            // 
+            // gTxtUser
+            // 
+            this.gTxtUser.BorderRadius = 8;
+            this.gTxtUser.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gTxtUser.DefaultText = "";
+            this.gTxtUser.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.gTxtUser.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.gTxtUser.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gTxtUser.DisabledState.Parent = this.gTxtUser;
+            this.gTxtUser.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gTxtUser.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gTxtUser.FocusedState.Parent = this.gTxtUser;
+            this.gTxtUser.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gTxtUser.HoverState.Parent = this.gTxtUser;
+            this.gTxtUser.IconLeft = global::CapaPresentacion.Properties.Resources.Contacts;
+            this.gTxtUser.Location = new System.Drawing.Point(75, 161);
+            this.gTxtUser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gTxtUser.Name = "gTxtUser";
+            this.gTxtUser.PasswordChar = '\0';
+            this.gTxtUser.PlaceholderForeColor = System.Drawing.Color.Red;
+            this.gTxtUser.PlaceholderText = "Usuario";
+            this.gTxtUser.SelectedText = "";
+            this.gTxtUser.ShadowDecoration.Parent = this.gTxtUser;
+            this.gTxtUser.Size = new System.Drawing.Size(300, 47);
+            this.gTxtUser.TabIndex = 18;
+            // 
+            // gunaPictureBox2
+            // 
+            this.gunaPictureBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox2.Image = global::CapaPresentacion.Properties.Resources.cactus;
+            this.gunaPictureBox2.Location = new System.Drawing.Point(233, 154);
+            this.gunaPictureBox2.Name = "gunaPictureBox2";
+            this.gunaPictureBox2.Size = new System.Drawing.Size(85, 194);
+            this.gunaPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaPictureBox2.TabIndex = 1;
+            this.gunaPictureBox2.TabStop = false;
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox1.Image = global::CapaPresentacion.Properties.Resources.Character_working_laptop_sitting_chair;
+            this.gunaPictureBox1.Location = new System.Drawing.Point(3, 64);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(251, 249);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaPictureBox1.TabIndex = 0;
+            this.gunaPictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(360, 37);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Biblioteca Comunitaria";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(929, 688);
+            this.ClientSize = new System.Drawing.Size(837, 468);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gTxtUser);
+            this.Controls.Add(this.gTxtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ToggleSwitch1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.gunaElipsePanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.gunaElipsePanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel1;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2ToggleSwitch guna2ToggleSwitch1;
+        private Guna.UI2.WinForms.Guna2TextBox gTxtUser;
+        private Guna.UI2.WinForms.Guna2TextBox gTxtPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
