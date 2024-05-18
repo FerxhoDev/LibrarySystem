d99f7fd6abaa3958b00ce7b64586faae7d7fa9f2
FerxhoDevl <ferdycard0na11@gmail.com>
2024-05-18 14:14:22 -0600

    Form Donaciones iniciado

---
+++ CapaPresentacion/Donaciones.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace CapaPresentacion
+{
+    partial class Donaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Donaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(1554, 817);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Donaciones";
+            this.Text = "Donaciones";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
